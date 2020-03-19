87dbbc43bbeec4a2153e66d2e87295e34461de1e
Aditya Prakash <adityaprakash185@gmail.com>
2020-03-19 10:21:38 +0530

    Navbar Completed

---
+++ src/components/MainComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import { BrowserRouter } from 'react-router-dom';
+
+class Main extends Component {
+
+    render(){
+        return (
+            <BrowserRouter>
+
+                <div>
+            
+                        <Header />
+           
+                   </div>
+            </BrowserRouter>
+           
+          );
+          }
+        }
+export default Main;
+        
+    
