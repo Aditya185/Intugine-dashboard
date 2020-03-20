62452974edea5a66ab740f296a6ac9c791e280b7
Aditya Prakash <adityaprakash185@gmail.com>
2020-03-20 22:11:48 +0530

    on clicking table row data fetched to timeline

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,11 +11,25 @@ import axios from 'axios';
 
 
 class Main extends Component {
+ 
+  constructor(props){
+    super(props)
+    
+  }
 
     state = {
         orders: [],
         scan_rev: []
       }
+
+      fetchDetails = (order) => {
+ 
+        console.log('We need to get the details for ', order);
+        console.log(order.scan)
+      
+      
+      }
+      
 
       
 
@@ -24,15 +38,17 @@ class Main extends Component {
         .then(res=>{
                 const orders = res.data.data;
                 const scans = res.data.data[0].scan;
-                const scan_rev = scans.reverse();
+               const scan_rev = scans.reverse();
                 // console.log(orders[0])
                 this.setState({orders})
                 this.setState({scan_rev})
-                console.log(this.state.scascan_rev)
+                // console.log(this.state.scascan_rev)
                 console.log(this.state.orders)
     
                
       })
+
+     
           
       }
 
@@ -40,10 +56,10 @@ class Main extends Component {
         return this.state.orders.map((order, index) => {
            // const { order.extra_fields.expected_delivery_date}=order;
            const {_id, awbno, carrier, from, to ,pickup_date , extra_fields,current_status} = order
-          
+         
            return (
            
-              <tr key={_id}>
+              <tr key={_id}  >
                 
                  <td>{awbno}</td>
                  <td>{carrier}</td>
@@ -66,13 +82,13 @@ class Main extends Component {
           <TimelineItem>
           <p className="timeline_content">
             <span className="timeline_status">
-              {time}
+              {status_detail}
           </span>
             <span className="timeline_date">
-              {location}
+              {time}
           </span>
             <span className="timeline_time">
-              {status_detail}
+              {location}
           </span>
           </p>
         </TimelineItem>
@@ -87,7 +103,24 @@ class Main extends Component {
     
 
     render(){
-
+       const a =this.state.orders;
+       let del =0,int=0,ood=0,dex=0,nfi=0;
+       if(this.state.orders){
+         a.map(data => data.current_status_code === 'DEL' ? del=del+1:del=del)
+       }
+       if(this.state.orders){
+        a.map(data => data.current_status_code === 'INT' ? int=int+1:int=int)
+      }
+      if(this.state.orders){
+        a.map(data => data.current_status_code === 'OOD' ? ood=ood+1:ood=ood)
+      }
+      if(this.state.orders){
+        a.map(data => data.current_status_code === 'DEX' ? dex=dex+1:dex=dex)
+      }
+       if(this.state.orders){
+        a.map(data => data.current_status_code === 'NFI' ? nfi=nfi+1:nfi=nfi)
+      }
+    
         const TimelineList = () => {
             return(
               
@@ -122,38 +155,36 @@ class Main extends Component {
             
                         <Header />
                
-  
+                        <div className="cards">
+    <div className="card">
+      <p className="card_head">DEL</p>
+        <p className="card_no">{del}</p >
+    </div>
+    <div className="card">
+      <p className="card_head">INT</p>
+        <p className="card_no">{int}</p >
+    </div>
+    <div className="card">
+      <p className="card_head">OOD</p>
+        <p className="card_no">{ood}</p >
+    </div>
+    <div className="card">
+      <p className="card_head">DEX</p>
+        <p className="card_no">{dex}</p >
+    </div>
+    <div className="card">
+      <p className="card_head">NFI</p>
+      <p className="card_no">{nfi}</p >
+    </div>
+   </div>
     
-    <div className="cards">
-          <div className="card">
-            <p className="card_head">DEL</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">INT</p>
-            <p className="card_no">232</p >
-          </div>
-          <div className="card">
-            <p className="card_head">OOD</p>
-            <p className="card_no">342</p >
-          </div>
-          <div className="card">
-            <p className="card_head">DEX</p>
-            <p className="card_no">916</p >
-          </div>
-          <div className="card">
-            <p className="card_head">NFI</p>
-            <p className="card_no">916</p >
-          </div>
-        </div>
-        
-        
+   
         
      
     
+   <Center/>
 
-
-    <div class="row row-content ">
+    {/* <div class="row row-content ">
        
     <div class="col-12 col-sm-6">
         <TimelineList/>
@@ -162,7 +193,7 @@ class Main extends Component {
         <Center/>
     </div>
 
-    </div>
+    </div> */}
            
     
 
